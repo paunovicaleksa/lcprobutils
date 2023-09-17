97f37b53a7716446b39b8df775898d25d30dac48
Paun <pa200202d@student.etf.bg.ac.rs>
2023-09-17 16:17:53 +0200

    added basic functionality

--- src/lcmain.go
+++ src/lcmain.go
@@ -1,21 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"os"
 )
 
 func main() {
-	file, err := os.OpenFile("problems.json", os.O_CREATE|os.O_RDWR, 0644)
+
+	addPtr := flag.Bool("add", false, "add a problem to the list/file")
+	listPtr := flag.Bool("list", false, "list all problems, sorted by urgency")
+	removePtr := flag.Int("remove", -1, "remove a problem with given index, as seen in list")
+	filePtr := flag.String("file", "problems.json", "path to file, default: problems.json")
+	/* toggle interactive mode? */
+	interactivePtr := flag.Bool("interactive", false, "toggle interactive mode")
+
+	flag.Parse()
+
+	file, err := os.OpenFile(*filePtr, os.O_CREATE|os.O_RDWR, 0644)
 
 	if err != nil {
 		log.Fatal("Error opening file")
 	}
 
 	defer file.Close()
+	defer write(file)
 	/* cli parsing here? */
 	read(file)
-	fmt.Println(lcproblems)
-	write(file)
+
+	/* only one at the same time? */
+	if *addPtr {
+		parseAdd()
+	} else if *listPtr {
+		printList()
+	} else if *removePtr >= 0 && *removePtr < len(lcproblems) {
+		remove(*removePtr)
+	} else if *interactivePtr {
+		/* TODO: tui */
+	}
 }
--- src/lcprobs.go
+++ src/lcprobs.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+	"sort"
+	"strings"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 const (
@@ -18,7 +23,7 @@ type Lcproblem struct {
 	Name       string    `json:"name"`
 	Added      time.Time `json:"added"` /* calculate priority */
 	Difficulty int       `json:"difficulty"`
-	Solved     bool      `json:"solved"` /* solved problems instantly have LOW priority, but should remain until deleted? */
+	// Solved     bool      `json:"solved"` /* solved problems instantly have LOW priority, but should remain until deleted? */
 }
 
 var lcproblems []Lcproblem
@@ -26,14 +31,17 @@ var lcproblems []Lcproblem
 func write(file *os.File) error {
 	outstr, err := json.Marshal(lcproblems)
 	if err != nil {
-		log.Fatal("error marshalling")
 		return err
 	}
 
-	file.Truncate(0)
-	file.WriteAt(outstr, 0)
+	err = file.Truncate(0)
+	if err != nil {
+		return err
+	}
 
-	return nil
+	_, err = file.WriteAt(outstr, 0)
+
+	return err
 }
 
 func read(file *os.File) error {
@@ -43,7 +51,84 @@ func read(file *os.File) error {
 		return err
 	}
 
-	json.Unmarshal(bytes, &lcproblems)
+	err = json.Unmarshal(bytes, &lcproblems)
+
+	if err != nil {
+		return err
+	}
+
+	sort.Slice(lcproblems, func(i, j int) bool {
+		return (lcproblems[i].Added.Nanosecond() * (lcproblems[i].Difficulty + 1)) > (lcproblems[j].Added.Nanosecond() * (lcproblems[j].Difficulty + 1))
+	})
 
 	return nil
+}
+
+func remove(index int) {
+	lcproblems[index] = lcproblems[len(lcproblems)-1]
+	lcproblems = lcproblems[:len(lcproblems)-1]
+}
+
+func parseAdd() {
+	var problem Lcproblem
+	reader := bufio.NewScanner(os.Stdin)
+	fmt.Print("problem name: ")
+	reader.Scan()
+	problem.Name = reader.Text()
+	easy := color.GreenString("Easy")
+	medium := color.YellowString("Medium")
+	hard := color.RedString("Hard")
+	fmt.Printf("problem difficulty (%v, %v, %v): ", easy, medium, hard)
+out:
+	for {
+		reader.Scan()
+		switch strings.ToLower(reader.Text()) {
+		case "easy":
+			problem.Difficulty = Easy
+			break out
+		case "medium":
+			problem.Difficulty = Medium
+			break out
+		case "hard":
+			problem.Difficulty = Hard
+			break out
+		default:
+			fmt.Println("invalid problem difficulty (Easy, Medium, Hard)")
+			fmt.Printf("problem difficulty (%v, %v, %v): ", easy, medium, hard)
+			continue
+		}
+	}
+
+	problem.Added = time.Now()
+
+	lcproblems = append(lcproblems, problem)
+}
+
+func printList() {
+	fmt.Println("Problem Name            Added On      Difficulty")
+	for _, problem := range lcproblems {
+		var name string
+		if len(problem.Name) > 15 {
+			name = problem.Name[:15]
+			name = name + "[...]"
+		} else {
+			name = problem.Name
+		}
+
+		var difficulty string
+		switch problem.Difficulty {
+		case Easy:
+			difficulty = color.GreenString("Easy")
+		case Medium:
+			difficulty = color.YellowString("Medium")
+		case Hard:
+			difficulty = color.RedString("Hard")
+		default:
+			difficulty = ""
+		}
+
+		dateFormatted := problem.Added.Format("Jan 02, 2006")
+
+		fmt.Printf("%-20.20v   %v   %v\n", name, dateFormatted, difficulty)
+	}
 }
